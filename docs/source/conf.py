f40c61b9eda43b61bd0bb7d7ea4c6a07692d6148
Jim Crist <jiminy.crist@gmail.com>
2019-01-14 12:27:17 -0600

    Initial sphinx source

---
+++ docs/source/conf.py
@@ -0,0 +1,47 @@
+import alabaster
+
+# Project settings
+project = 'Deploying JupyterHub on Hadoop'
+copyright = '2019, Jim Crist'
+author = 'Jim Crist'
+release = version = '0.1.0'
+
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+pygments_style = 'sphinx'
+exclude_patterns = []
+
+# Sphinx Extensions
+extensions = ['sphinx.ext.extlinks']
+
+numpydoc_show_class_members = False
+
+extlinks = {
+    'issue': ('https://github.com/jcrist/jupyterhub-on-hadoop/issues/%s', 'Issue #'),
+    'pr': ('https://github.com/jcrist/jupyterhub-on-hadoop/pull/%s', 'PR #')
+}
+
+# Sphinx Theme
+html_theme = 'alabaster'
+html_theme_path = [alabaster.get_path()]
+templates_path = ['_templates']
+html_static_path = ['_static']
+html_theme_options = {
+    'description': 'Documentation for deploying JupyterHub on a Hadoop Cluster',
+    'github_button': True,
+    'github_count': False,
+    'github_user': 'jcrist',
+    'github_repo': 'jupyterhub-on-hadoop',
+    'travis_button': True,
+    'show_powered_by': False,
+    'page_width': '960px',
+    'sidebar_width': '250px',
+    'code_font_size': '0.8em'
+}
+html_sidebars = {
+    '**': ['about.html',
+           'navigation.html',
+           'help.html',
+           'searchbox.html']
+}
